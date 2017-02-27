be337a8826163a43fb16972e625e085f467a3af8
brimzi <brimzi@gmail.com>
2017-02-27 22:44:47 +0100

    Protobuf definition and clean up:

---
+++ modules/iot-core/src/main/java/brimzi/interithcloud/iotcore/App.java
@@ -0,0 +1,7 @@
+package brimzi.interithcloud.iotcore;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
--- modules/mqtt-frontend/src/main/kotlin/brimzi/interothcloud/mqttfrontend/App.kt
+++ modules/mqtt-frontend/src/main/kotlin/brimzi/interothcloud/mqttfrontend/App.kt
@@ -1,8 +1,14 @@
 package brimzi.interothcloud.mqttfrontend
 
+import io.grpc.ServerBuilder
+
 /**
  * Created by brimzy on 21/02/2017.
  */
 class App{
+
+}
+
+fun main(){
 
 }
